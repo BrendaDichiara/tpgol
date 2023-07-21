be9710c52cfdefa2d2194afe63126b582a7a0f90
LucioMansilla <luciomansillaztw@gmail.com>
2023-07-21 17:08:30 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 mkdir -p ../atomics/gol
 mkdir -p ../examples/gol
 mkdir -p ../patterns
+mkdir -p ../library/gol
 
 cp atomics/* ../atomics/gol
 cp examples/* ../examples/gol
 cp patterns/* ../patterns
+cp lib/* ../library/gol
