1aa27f5ecb3f59eb50c7eb16c48183cc932ed2ff
LucioMansilla <luciomansillaztw@gmail.com>
2023-07-26 01:52:31 -0300

    add gui

--- gen_txt.py
+++ gen_txt.py
@@ -10,8 +10,8 @@ class Grid:
 
         for i in range(rows):
             for j in range(cols):
-                self.buttons[i][j] = tk.Button(master, width=2, height=1, bg='white', command=lambda i=i, j=j: self.click(i, j))
-                self.buttons[i][j].grid(row=i+2, column=j)  # Start the grid on the third row
+                self.buttons[i][j] = tk.Button(master, width=2, height=1, bg='white', bd=1, relief="solid", command=lambda i=i, j=j: self.click(i, j)) 
+                self.buttons[i][j].grid(row=i+2, column=j, padx=1, pady=1)
 
     def click(self, i, j):
         self.cells[i][j] = 1 - self.cells[i][j]
@@ -37,7 +37,7 @@ class Application:
         self.button_create = tk.Button(master, text='Crear cuadrícula', command=self.create_grid)
         self.button_export = tk.Button(master, text='Exportar txt', command=self.export_grid)
 
-        self.entry_rows.grid(row=0, column=0, padx=5, pady=5)  # Add some padding around the entries
+        self.entry_rows.grid(row=0, column=0, padx=5, pady=5)
         self.entry_cols.grid(row=0, column=1, padx=5, pady=5)
         self.button_create.grid(row=0, column=2, padx=5, pady=5)
         self.button_export.grid(row=0, column=3, padx=5, pady=5)
