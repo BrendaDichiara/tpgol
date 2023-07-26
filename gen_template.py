ab3a9075e714727ca0f5a6162caa6e9da1d5f873
LucioMansilla <luciomansillaztw@gmail.com>
2023-07-26 01:49:40 -0300

    report added

---
+++ gen_template.py
@@ -0,0 +1,96 @@
+ndef generate_pdm_v6(N, M, output_file):
+    # Base content of the .pdm file
+    BASE_CONTENT = """
+Coupled
+    {{
+    Type = Root
+    Name = prueba_{N}x{M}
+    Ports = 0; 0
+    Description = 
+    Graphic
+        {{
+        Position = 0; 0
+        Dimension = 600; 600
+        Direction = Right
+        Color = 15
+        Icon = 
+        Window = 5000; 5000; 5000; 5000
+        }}
+    Parameters
+        {{
+        }}
+    System
+        {{
+    """.format(N=N, M=M)
+
+    # Base templates for different sections of the .pdm file
+    MEDIATOR_TEMPLATE = """
+            \t\t Atomic
+            {{
+            Name = Mediator0
+            Ports = {num_ports} ; 1
+            Path = gol/mediator.h
+            Description = Atomic DEVS model
+            Graphic
+                {{
+                Position = -15780 ; -8850
+                Dimension = 675 ; 1350
+                Direction = Right
+                Color = 4
+                Icon = None
+                }}
+            Parameters
+                {{
+                filename = Str; initial ; str
+                }}
+            }}
+    """
+
+    SENDER_TEMPLATE = """
+        \t\t Atomic
+            {{
+            Name = Sender{sender_id}
+            Ports = 1 ; 1
+            Path = gol/cell.h
+            Description = Atomic DEVS model
+            Graphic
+                {{
+                Position = -17880 ; {position}
+                Dimension = 675 ; 720
+                Direction = Right
+                Color = 15
+                Icon = None
+                }}
+            Parameters
+                {{
+                CID = Str; {sender_id} ; int
+                }}
+            }}
+    """
+
+    # Start with the base content and open the 'System' section
+    new_content = BASE_CONTENT.strip() + "\n\t\t"
+
+    # Add the MEDIATOR
+    new_content += MEDIATOR_TEMPLATE.format(num_ports=N*M).strip() + "\n\t\t"
+    # Add each SENDER
+    for i in range(N*M):
+            new_content += SENDER_TEMPLATE.format(sender_id=i, position=-9510 + 1425*i).strip() + "\n\t\t"
+        
+    # Close the 'System' section and the rest of the file
+    new_content += "        }\n    }\n"
+
+    # Write the new .pdm file
+    with open(output_file, "w") as file:
+        file.write(new_content)
+
+# Set the size of the board
+N = 10
+M = 10
+
+# Output file path
+output_file = "prueba_{0}x{1}.pdm".format(N, M)
+
+# Generate the .pdm file
+generate_pdm_v6(N, M, output_file)
+
---
+++ gen_txt.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import filedialog
+
+class Grid:
+    def __init__(self, master, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.cells = [[0 for _ in range(cols)] for _ in range(rows)]
+        self.buttons = [[None for _ in range(cols)] for _ in range(rows)]
+
+        for i in range(rows):
+            for j in range(cols):
+                self.buttons[i][j] = tk.Button(master, width=2, height=1, bg='white', command=lambda i=i, j=j: self.click(i, j))
+                self.buttons[i][j].grid(row=i+2, column=j)  # Start the grid on the third row
+
+    def click(self, i, j):
+        self.cells[i][j] = 1 - self.cells[i][j]
+        self.buttons[i][j].config(bg='black' if self.cells[i][j] else 'white')
+
+    def export(self):
+        filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text file", "*.txt"),("All Files", "*.*")))
+        with open(filename, 'w') as file:
+            file.write(str(self.rows) + '\n')
+            file.write(str(self.cols) + '\n')
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    file.write(str(self.cells[i][j]))
+                file.write('\n')
+
+class Application:
+    def __init__(self, master):
+        self.master = master
+        self.grid = None
+
+        self.entry_rows = tk.Entry(master)
+        self.entry_cols = tk.Entry(master)
+        self.button_create = tk.Button(master, text='Crear cuadrícula', command=self.create_grid)
+        self.button_export = tk.Button(master, text='Exportar txt', command=self.export_grid)
+
+        self.entry_rows.grid(row=0, column=0, padx=5, pady=5)  # Add some padding around the entries
+        self.entry_cols.grid(row=0, column=1, padx=5, pady=5)
+        self.button_create.grid(row=0, column=2, padx=5, pady=5)
+        self.button_export.grid(row=0, column=3, padx=5, pady=5)
+
+    def create_grid(self):
+        rows = int(self.entry_rows.get())
+        cols = int(self.entry_cols.get())
+        if self.grid is not None:
+            for button_row in self.grid.buttons:
+                for button in button_row:
+                    button.destroy()
+        self.grid = Grid(self.master, rows, cols)
+
+    def export_grid(self):
+        if self.grid is not None:
+            self.grid.export()
+
+root = tk.Tk()
+app = Application(root)
+root.mainloop()
